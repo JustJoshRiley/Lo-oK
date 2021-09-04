74a4851253c4ddfef4a30383669c4f8a9029314d
justjoshriley <joshriley316@gmail.com>
2021-09-03 23:11:29 -0700

    test calling api route

--- hello/static/index.js
+++ hello/static/index.js
@@ -2,4 +2,4 @@ async function get_urls(){
     return await fetch('/results').then(response => response.json());
 };
 
-await get_urls().then(urls => alert(urls['urls'][0]));s
+get_urls().then(urls => alert(urls['urls'][0]));
