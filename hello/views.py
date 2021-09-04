97ae01c90a0bf29931cc80a6f26fa21bae2e56b2
Evan <evanwall@buffalo.edu>
2021-09-03 22:12:53 -0700

    add api route for search results

--- hello/views.py
+++ hello/views.py
@@ -3,11 +3,17 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import json
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
+def results(request):
+    response_data = {}
+    response_data['urls'] = ['joshriley.tech', 'walley892..github.io']
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 def db(request):
 
