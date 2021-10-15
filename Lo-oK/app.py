db03d65179083fae4b3bc1238bd953341233e8d6
justjoshriley <joshriley316@gmail.com>
2021-10-15 13:07:11 -0700

    added reademe

--- Lo-oK/app.py
+++ Lo-oK/app.py
@@ -1,30 +1,129 @@
 from flask import Flask, request, render_template
-
+from flask_pymongo import PyMongo
 from googleapiclient.discovery import build
-
-from flask import jsonify
+from flask import make_response
 
 import pprint
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://root:thisisSparta!@srv-captain--look-database/mydatabase?authSource=admin"
-# app.config["MONGO_URI"] = "mongodb://root:thisisSparta!@srv-captain--look-database:27017/mydatabase?authSource=admin"
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/db"
+mongo = PyMongo(app)
 
 
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/response', methods=['GET', 'POST'])
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+
+    if request.method == 'POST':
+        new_user = {
+            "username":"",
+            "password":"",
+        }
+        
+
+        new_user["username"] = request.form.get("username")
+        new_user["password"] = request.form.get("password")
+        confirmPassword = request.form.get('confirmPassword')
+        
+        # if the user successfully confirms their password
+        if new_user["password"] == confirmPassword:
+            # find if the user exists in the database
+            # if the user exists, route them to log in
+            if mongo.db.user.count_documents({"username": new_user["username"]}) == 1:
+                return render_template('login.html', **{
+                    'username': new_user["username"],
+                })
+            # if the user is not, put them in the database
+            else :
+                mongo.db.user.insert(new_user)
+                return render_template('registered.html', **{
+                    'username': new_user["username"],
+                })
+        else :
+            pass
+    else :
+        return render_template('404.html')
+
+@app.route('/handleLoggedInUser', methods = ['POST'])
+def handleLoggedInUser():
+    # check the post data
+    # return logged in page
+    user = {
+        'username' : "",
+        'password' : "" 
+    }
+
+    user["username"] = request.form.get("username")
+    user["password"] = request.form.get("password")
+
+    print(user["username"])
+    print(user["password"])
+
+    # find the username in the datatbase
+    username_object = mongo.db.user.find({"username": user["username"]})
+
+    # get the user object
+    user_object = username_object.next()
+
+    if user_object["password"] == user["password"] and user_object["username"] == user["username"]:
+        return render_template('registered.html', **{
+                'username': user_object["username"],
+        })
+    else :
+        pass
+
+    
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+
+    if request.method == 'POST':
+        user = {
+            "username": "",
+            "password": ""
+        }
+
+        
+        user["username"] = request.form.get("username2")
+        user["password"] = request.form.get("password2")
+        # print(user["username"])
+        # print(user["password"])
+
+        # find the username in the database
+        username_object = mongo.db.user.find({"username": user["username"]})
+        # get the user object
+        user_object = username_object.next()
+        # if the password matches the object password
+        if user_object["password"] == user["password"] and user_object["username"] == user["username"]:
+            # take the user to registered
+            # print(user_object["username"], user_object["password"])
+            resp = make_response(render_template('registered.html', **{
+                'username': user_object["username"],
+            }))
+            resp.set_cookie('username', user["username"])
+            resp.set_cookie('password', user["password"])
+            return resp
+        else :
+            return("nothing yet")
+    else :
+        return render_template('404.html')
+
+
+@app.route('/response', methods=['GET','POST'])
 def getResponse():
     
     if request.method == 'POST':
         search = request.form.get("response")
-        print(search)
         
         service = build("customsearch", "v1", 
-            developerKey="AIzaSyDmMPBhep_IvCp1W0XiD_O0Pf36WSWsWT8")
+            developerKey="AIzaSyDgzp6gO1foZqVJNifc_mPrG0CXmi5mv0Q")
 
         res = service.cse().list(
             q=search, 
@@ -45,6 +144,10 @@ def getResponse():
         print(newDict)
 
         return render_template('results.html', newDict=newDict)
+    else :
+        return render_template('404search.html')
+
+
 
 
 if __name__ == '__main__':
---
+++ Lo-oK/readme.md
@@ -0,0 +1,24 @@
+# Lo-oK
+
+Look is a lightweight Python application that allows users to search the web and receive results without the hassles of current searching browsers.
+
+## Installation
+
+None Required [Start Searching Now](http://look.dev.justjoshriley.com/).
+
+##
+
+## Made With:
+https://img.shields.io/badge/<Backend>-<Python>-<Yellow>
+https://img.shields.io/badge/<Frontend>-<Javascript>-<Green>
+https://img.shields.io/badge/<Databse>-<MongoDb>-<White>
+
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
